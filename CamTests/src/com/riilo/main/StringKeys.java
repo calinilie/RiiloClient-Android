f675846e98b725c31b540ecccae4ba825ec0dec5
Calin <calinilielaur@gmail.com>
2013-11-26 20:07:33 +0100

    renamed stringKeys to com.riilo.main

--- CamTests/src/com/riilo/main/StringKeys.java
+++ CamTests/src/com/riilo/main/StringKeys.java
@@ -2,39 +2,39 @@ package com.riilo.main;
 
 public class StringKeys {
 
-	public static final String POST_BUNDLE = "org.calin.camtests.StringKeys.POST_INTENT_BUNDLE";
-	public static final String POST_LIST_PARCELABLE = "org.calin.camtests.StringKeys.POST_LIST_PARCELABLE";
-	public static final String POST_LIST_RESULT_RECEIVER = "org.calin.camtests.StringKeys.POST_LIST_RESULT_RECEIVER";
-	public static final String POST_RESULT_RECEIVER_TYPE = "org.calin.camtests.StringKeys.POST_LIST_RESULT_RECEIVER_TYPE";
-	public static final String POST_RESULT_RECEIVER_NOTIFICATION_NUMBER = "org.calin.camtests.StringKeys.POST_RESULT_RECEIVER_NOTIFICATION_NUMBER";
-	public static final String POST_RESULT_RECEIVER_NEARBY_POSTS_NUMBER = "org.calin.camtests.StringKeys.POST_RESULT_RECEIVER_NEARBY_POSTS_NUMBER";
+	public static final String POST_BUNDLE = "com.riilo.main.StringKeys.POST_INTENT_BUNDLE";
+	public static final String POST_LIST_PARCELABLE = "com.riilo.main.StringKeys.POST_LIST_PARCELABLE";
+	public static final String POST_LIST_RESULT_RECEIVER = "com.riilo.main.StringKeys.POST_LIST_RESULT_RECEIVER";
+	public static final String POST_RESULT_RECEIVER_TYPE = "com.riilo.main.StringKeys.POST_LIST_RESULT_RECEIVER_TYPE";
+	public static final String POST_RESULT_RECEIVER_NOTIFICATION_NUMBER = "com.riilo.main.StringKeys.POST_RESULT_RECEIVER_NOTIFICATION_NUMBER";
+	public static final String POST_RESULT_RECEIVER_NEARBY_POSTS_NUMBER = "com.riilo.main.StringKeys.POST_RESULT_RECEIVER_NEARBY_POSTS_NUMBER";
 	public static final int POST_RESULT_RECEIVER_CODE_UPDATE_ADAPTER_DESC = 123;
 	public static final int POST_RESULT_RECEIVER_CODE_UPDATE_ADAPTER_ASC = 122;
 	public static final int POST_RESULT_RECEIVER_CODE_UPDATE_VIEW  = 121;
 	public static final int POST_RESULT_RECEIVER_CODE_UPDATE_VIEW_AND_ADAPTER = 120;
-	public static final String CONVERSATION_FROM_CONVERSATION_ID = "org.calin.camtests.StringKeys.CONVERSATION_FROM_CONVERSATION_ID";
-	public static final String NOTIFICATIONS_USER_ID="org.calin.camtests.StringKeys.NOTIFICATIONS_USER_ID";
-	public static final String NOTIFICATION_SILENCE_CONVERSATION_ID = "org.calin.camtests.StringKeys.NOTIFICATION_SILENCE_CONVERSATION_ID";
-	public static final String NOTIFICATION_SILENCE_USER_ID = "org.calin.camtests.StringKeys.NOTIFICATION_SILENCE_USER_ID";
-	public static final String NEARBY_POSTS_LATITUDE = "org.calin.camtests.StringKeys.NEARBY_POSTS_LATITUDE";
-	public static final String NEARBY_POSTS_LONGITUDE = "org.calin.camtests.StringKeys.NEARBY_POSTS_LONGITUDE";
-	public static final String WAS_TUTORIAL_RUN = "org.calin.camtests.StringKeys.WAS_TUTORIAL_RUN";
+	public static final String CONVERSATION_FROM_CONVERSATION_ID = "com.riilo.main.StringKeys.CONVERSATION_FROM_CONVERSATION_ID";
+	public static final String NOTIFICATIONS_USER_ID="com.riilo.main.StringKeys.NOTIFICATIONS_USER_ID";
+	public static final String NOTIFICATION_SILENCE_CONVERSATION_ID = "com.riilo.main.StringKeys.NOTIFICATION_SILENCE_CONVERSATION_ID";
+	public static final String NOTIFICATION_SILENCE_USER_ID = "com.riilo.main.StringKeys.NOTIFICATION_SILENCE_USER_ID";
+	public static final String NEARBY_POSTS_LATITUDE = "com.riilo.main.StringKeys.NEARBY_POSTS_LATITUDE";
+	public static final String NEARBY_POSTS_LONGITUDE = "com.riilo.main.StringKeys.NEARBY_POSTS_LONGITUDE";
+	public static final String WAS_TUTORIAL_RUN = "com.riilo.main.StringKeys.WAS_TUTORIAL_RUN";
 	
-	public static final String POST_PIC_URI ="org.calin.camtests.StringKeys.POST_PIC_URI";
-	public static final String POST_LATITUDE = "org.calin.camtests.StringKeys.POST_LATITUDE";
-	public static final String POST_LONGITUDE = "org.calin.camtests.StringKeys.POST_LONGITUDE";
-	public static final String POST_MESSAGE = "org.calin.camtests.StringKeys.POST_MESSAGE";
-	public static final String POST_ACCURACY = "org.calin.camtests.StringKeys.POST_ACCURACY";
-	public static final String POST_DATE_CREATED = "org.calin.camtests.StringKeys.POST_DATE_CREATED";
-	public static final String POST_USER_AT_LOCATION = "org.calin.camtests.StringKeys.POST_USER_AT_LOCATION";
-	public static final String POST_USER_ID = "org.calin.camtests.StringKeys.POST_USER_ID";
-	public static final String POST_ID = "org.calin.camtests.StringKeys.POST_ID";
-	public static final String POST_REPLIES_TO_POSTID = "org.calin.camtests.StringKeys.POST_REPLIES_TO_POSTID";
-	public static final String POST_CONVERSATION_ID = "org.calin.camtests.StringKeys.POST_CONVERSATION_ID";
-	public static final String POST_ORIGIN_LATITUDE = "org.calin.camtests.StringKeys.POST_ORIGIN_LATITUDE";
-	public static final String POST_ORIGIN_LONGITUDE = "org.calin.camtests.StringKeys.POST_ORIGIN_LONGITUDE";
+	public static final String POST_PIC_URI ="com.riilo.main.StringKeys.POST_PIC_URI";
+	public static final String POST_LATITUDE = "com.riilo.main.StringKeys.POST_LATITUDE";
+	public static final String POST_LONGITUDE = "com.riilo.main.StringKeys.POST_LONGITUDE";
+	public static final String POST_MESSAGE = "com.riilo.main.StringKeys.POST_MESSAGE";
+	public static final String POST_ACCURACY = "com.riilo.main.StringKeys.POST_ACCURACY";
+	public static final String POST_DATE_CREATED = "com.riilo.main.StringKeys.POST_DATE_CREATED";
+	public static final String POST_USER_AT_LOCATION = "com.riilo.main.StringKeys.POST_USER_AT_LOCATION";
+	public static final String POST_USER_ID = "com.riilo.main.StringKeys.POST_USER_ID";
+	public static final String POST_ID = "com.riilo.main.StringKeys.POST_ID";
+	public static final String POST_REPLIES_TO_POSTID = "com.riilo.main.StringKeys.POST_REPLIES_TO_POSTID";
+	public static final String POST_CONVERSATION_ID = "com.riilo.main.StringKeys.POST_CONVERSATION_ID";
+	public static final String POST_ORIGIN_LATITUDE = "com.riilo.main.StringKeys.POST_ORIGIN_LATITUDE";
+	public static final String POST_ORIGIN_LONGITUDE = "com.riilo.main.StringKeys.POST_ORIGIN_LONGITUDE";
 	
-	public static final String WS_INTENT_TYPE = "org.calin.camtests.StringKeys.WORKER_SERVICE_INTENT_TYPE";
+	public static final String WS_INTENT_TYPE = "com.riilo.main.StringKeys.WORKER_SERVICE_INTENT_TYPE";
 	public static final int WS_INTENT_POST = 1;
 //	public static final int WS_INTENT_TAKE_PICTURE = 2;
 	public static final int WS_INTENT_GET_LATEST_POSTS = 3;
@@ -43,15 +43,15 @@ public class StringKeys {
 	public static final int WS_INTENT_NOTIFICATIONS_SILENCE = 6;
 	public static final int WS_INTENT_NEARBY_POSTS = 7;
 	
-	public static final String TAKE_PICTURE_INTENT_START_DATETIME = "org.calin.camtests.StringKeys.dateCameraIntentStarted";
-	public static final String TAKE_PICTURE_FILE_NAME_AND_PATH= "org.calin.camtests.StringKeys.FILE_NAME_AND_PATH";
-	public static final String TAKE_PICTURE_PIC_URI = "org.calin.camtests.StringKeys.CAMERA_PIC_URI_STATE";
+	public static final String TAKE_PICTURE_INTENT_START_DATETIME = "com.riilo.main.StringKeys.dateCameraIntentStarted";
+	public static final String TAKE_PICTURE_FILE_NAME_AND_PATH= "com.riilo.main.StringKeys.FILE_NAME_AND_PATH";
+	public static final String TAKE_PICTURE_PIC_URI = "com.riilo.main.StringKeys.CAMERA_PIC_URI_STATE";
 	public static final int TAKE_PICTURE_REQUEST_CODE = 100; 
 	
 	public static final int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
-	public static final String CONNECTION_TIMEDOUT = "org.calin.camtests.StringKeys.CONNECTION_TIMEDOUT";
+	public static final String CONNECTION_TIMEDOUT = "com.riilo.main.StringKeys.CONNECTION_TIMEDOUT";
 //	public static final int CONNECTION_TIMEDOUT_AS_INT = 412;
-	public static final String POST_REQUEST_FAILED = "org.calin.camtests.StringKeys.POST_REQUEST_FAILED";
+	public static final String POST_REQUEST_FAILED = "com.riilo.main.StringKeys.POST_REQUEST_FAILED";
 
 	
 	
