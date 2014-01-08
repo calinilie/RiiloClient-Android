a7f3602e96a9e7d46cf42c55fa28f87d1d5af0fb
Calin <calinilielaur@gmail.com>
2014-01-08 02:51:43 +0100

    swipe tip in tutorial

--- CamTests/src/com/riilo/tutorial/TutorialFragment.java
+++ CamTests/src/com/riilo/tutorial/TutorialFragment.java
@@ -9,13 +9,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.view.animation.Animation;
+import android.view.animation.Animation.AnimationListener;
+import android.view.animation.AnimationUtils;
 import android.view.ViewGroup;
+import android.webkit.WebView.FindListener;
 import android.widget.Button;
 
-public class TutorialFragment extends Fragment implements OnClickListener{
+public class TutorialFragment extends Fragment implements OnClickListener, AnimationListener{
 
 	private boolean isLastPage;
 	private int position;
+	private View view;
 	
 	public void setIsLastPage(boolean isLastPage){
 		this.isLastPage = isLastPage;
@@ -65,6 +70,11 @@ public class TutorialFragment extends Fragment implements OnClickListener{
     		((Button)rootView.findViewById(R.id.button_end_tutorial)).setVisibility(View.VISIBLE);
     		((Button)rootView.findViewById(R.id.button_end_tutorial)).setOnClickListener(this);
     	}
+    	else if (position==0){
+    		rootView.findViewById(R.id.tutorial_layout_content0).setOnClickListener(this);
+    		rootView.findViewById(R.id.button_close_tutorial_swipe).setOnClickListener(this);
+    	}
+    	this.view = rootView;
         return rootView;
 	}
 
@@ -75,5 +85,57 @@ public class TutorialFragment extends Fragment implements OnClickListener{
 			activity.analytics.recordEvent_Tutorial_EndButtonClick();
 			activity.finish();
 		}
+		else if (v.getId()==R.id.tutorial_layout_content0){
+			View tutorialSwipeContainer = view.findViewById(R.id.tutorial_tutorial_swipe_container);
+			
+			Animation fadeIn = AnimationUtils.loadAnimation(activity.getApplicationContext(),
+   	                R.anim.fade_in);
+   	   	 	
+	   	   	if (tutorialSwipeContainer.getVisibility()==View.GONE){
+	   	   		 tutorialSwipeContainer.startAnimation(fadeIn);
+	   	   		 tutorialSwipeContainer.requestLayout();
+	   	   		 tutorialSwipeContainer.setVisibility(View.VISIBLE);
+	   	   		 view.findViewById(R.id.tutorial_swipe).setVisibility(View.VISIBLE);
+	   	   	 }
+		}else if (v.getId() == R.id.button_close_tutorial_swipe){
+			View tutorialSwipe = view.findViewById(R.id.tutorial_swipe);
+			
+			Animation slideOut = AnimationUtils.loadAnimation(activity.getApplicationContext(),
+	                R.anim.slide_out_bottom);
+	   	 	slideOut.setAnimationListener(this);
+	   	 	
+	   	   	if (tutorialSwipe != null && tutorialSwipe.getVisibility()==View.VISIBLE){
+	   	   		tutorialSwipe.startAnimation(slideOut);
+	   	   		tutorialSwipe.requestLayout();
+	   			tutorialSwipe.setVisibility(View.GONE);
+	   	   	 }
+		}
+	}
+
+	@Override
+	public void onAnimationEnd(Animation animation) {
+		View tutorialSwipeContainer = view.findViewById(R.id.tutorial_tutorial_swipe_container);
+		
+		Animation fadeOut = AnimationUtils.loadAnimation(activity.getApplicationContext(),
+	                R.anim.fade_out);
+	   	 
+   	   	if (tutorialSwipeContainer != null && tutorialSwipeContainer.getVisibility()==View.VISIBLE){
+   	   		 tutorialSwipeContainer.startAnimation(fadeOut);
+   	   		 tutorialSwipeContainer.requestLayout();
+   	   		 tutorialSwipeContainer.setVisibility(View.GONE);
+   	   	 }
+		
+	}
+
+	@Override
+	public void onAnimationRepeat(Animation arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void onAnimationStart(Animation arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 }
